a7904568901d6391d980017fe788e09147e907dc
danigit <ds.acconto@gmail.com>
2018-10-17 00:59:19 +0200

    VIEW TYPES
    Aggiunto il popup confirm in fase di cancellazione di una tipologia
    
    view-types.js -> confirmDeleteType(param, param, param, param) - funzione che mostra un confirm popup che viene construito con i parametri passati alla funazione

--- www/js/view-types.js
+++ www/js/view-types.js
@@ -14,20 +14,21 @@ function seeTypes() {
                     //creo il pulsante per la cancellazione delle tipologie
                     let deleteElem = $('<a href="#" id="' + value['id'] + '" data-name="' + value['type'] + '" class="ui-icon-redminus border-red-1 border-radius-10">Elimina tipologia</a>').on('click', function () {
                         let parent = $(this).parent();
-                        console.log('inserting seccond: ' + key + '/' + value);
                         let deleteTypeForm = new FormData();
 
                         deleteTypeForm.append('id', value['id']);
 
-                        let deleteTypePromise = httpPost('php/ajax/delete_type.php', deleteTypeForm, 'POST');
+                        confirmDeleteType('Cancella tipologia', 'Sei sicuro di voler cancellare la tipologia?', 'Cancella tipologia', function () {
+                            let deleteTypePromise = httpPost('php/ajax/delete_type.php', deleteTypeForm, 'POST');
 
-                        deleteTypePromise.then(
-                            function (data) {
-                                if (data.result) {
-                                    $(parent).remove();
+                            deleteTypePromise.then(
+                                function (data) {
+                                    if (data.result) {
+                                        $(parent).remove();
+                                    }
                                 }
-                            }
-                        )
+                            );
+                        });
                         // $('#type-list-ul').append(inserRow(key, value));
                         // $('#type-list-ul').listview('refresh');
                     });
@@ -40,4 +41,13 @@ function seeTypes() {
             }
         }
     )
+}
+
+function confirmDeleteType(title, content, button, callback) {
+    $("#delete-type-confirm .delete-type-confirm-header").text(title);
+    $("#delete-type-confirm .delete-type-confirm-text").text(content);
+    $("#delete-type-confirm .delete-type-confirm-button").text(button).on("click.delete-type-confirm", function() {
+        callback();
+    });
+    $('#delete-type-confirm').popup('open');
 }
