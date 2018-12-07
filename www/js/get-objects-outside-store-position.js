f76e3c9b6d1f17b41ffab6f66735c439e03aa4fe
danigit <ds.acconto@gmail.com>
2018-12-07 02:10:48 +0100

    REFACTORING
    Refactored la pagina get-objects-outside-store-position, inserito le stringe per il multilingua

--- www/js/get-objects-outside-store-position.js
+++ www/js/get-objects-outside-store-position.js
@@ -1,42 +1,34 @@
 /**
- * Funzione
+ * Funzione che recupera la posizione degli oggetti fuori dal magazzino
  */
 function getObjectsOutsideStorePosition() {
 
+    //invio richiesta xmlhttp
     let allTagsPromise = httpPost('php/ajax/objects_outside_store_position.php', '', 'GET');
 
+    //interpreto risposta
     allTagsPromise.then(
         function (data) {
+            let objectsOutsideStoreBody = $('#objects-outside-store-body');
             //controllo se ci sono stati degli errori nella chiamata
             if (data.result) {
-                console.log(data);
-                $('#objects-outside-store-body').empty();
-                let tableRow;
-                let i = 0;
+                objectsOutsideStoreBody.empty();
+                let tableRow, i = 0;
                 $.each(data[0], function (key, value) {
-                    if((i++ % 2) === 0) {
-                        tableRow = $('<tr></tr>');
-                    }else{
-                        tableRow = $('<tr class="gray-background"></tr>')
-                    }
+                    tableRow = setTableRowColor(i++);
                     //elaboro le righe della tabella e le visualizzo
                     $.each(value, function (innerKey, innerValue) {
                         tableRow.append('<td class="font-x-large center-text width-50">' + innerValue + '</td>');
                     });
 
-                    $('#objects-outside-store-body').append(tableRow).trigger('create');
+                    objectsOutsideStoreBody.append(tableRow).trigger('create');
                 });
 
-                showEmptyTable($('#objects-outside-store-body'), 'Nessuna oggetto da mostrare');
+                showEmptyTable(objectsOutsideStoreBody, language['lan-see-kit-objects-no-object']);
             } else {
-                let allKitErrorMessage = $('#all-kit-error-message');
-
-                let message = $('<div class="center-text error-message"><span>' + data.message + '</span></div>');
-                if ($('.error-message').length !== 0)
-                    allKitErrorMessage.find('.error-message').remove();
-                allKitErrorMessage.append(message);
+                showEmptyTable(objectsOutsideStoreBody, language['no-server-response'] + ". <br> Error: " + data.message);
             }
         }
     );
-};
+}
 
